45b4d6f8f9f0a9c399a099d40f848702d18debcb
Mohammed Amin <coldprohor12@gmail.com>
2025-03-02 00:55:28 -0800

    change ip

--- client.go
+++ client.go
@@ -69,7 +69,7 @@ type Game struct {
 
 // Connect to WebSocket
 func (g *Game) connectWebSocket() {
-	u := url.URL{Scheme: "ws", Host: "10.40.134.115:8080", Path: "/ws"} // Replace with your server's IP
+	u := url.URL{Scheme: "ws", Host: "10.169.1.237:8080", Path: "/ws"} // Replace with your server's IP
 	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatal("WebSocket connection failed:", err)
